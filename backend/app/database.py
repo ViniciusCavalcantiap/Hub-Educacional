ada51fd8c61c87ee131796003b2d81e7ce7fb9ef
Vinícius Pena <vinicius.cavalcanti.ap@gmail.com>
2026-03-04 13:29:42 -0300

    fix: create data folder automatically to prevent sqlite CI error

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,9 +6,7 @@ DATABASE_URL = "sqlite:///./data/database.db"
 
 os.makedirs("data", exist_ok=True)
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
